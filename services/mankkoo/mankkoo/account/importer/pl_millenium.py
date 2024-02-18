fb4ad9ed47e9e6b63c3a243bd1e5609c66cca8cf
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2024-02-18 17:22:01 +0000

    refactor pl_millenium to pipeline

--- services/mankkoo/mankkoo/account/importer/pl_millenium.py
+++ services/mankkoo/mankkoo/account/importer/pl_millenium.py
@@ -4,12 +4,73 @@ import io
 import mankkoo.account.models as models
 import mankkoo.database as db
 
+
+def select_only_required_columns(df: pd.DataFrame) -> pd.DataFrame:
+    df = df[[
+        'Data transakcji',
+        'Odbiorca/Zleceniodawca',
+        'Opis',
+        'Obciążenia',
+        'Uznania',
+        'Waluta']]
+    return df
+
+
+def get_operation(df: pd.DataFrame) -> pd.DataFrame:
+    df['Operation'] = np.where(df['Obciążenia'] < 0, df['Obciążenia'], df['Uznania'])
+    return df
+
+
 def prepare_tile(df: pd.DataFrame) -> pd.DataFrame:
     df['Title'] = df['Odbiorca/Zleceniodawca'] + ' - ' + df['Opis']
     df['Title'] = df['Title'].str.replace(',', '')
     df['Title'] = df['Title'].str.replace(r'\s+', ' ', regex=True)
     df['Title'] = df['Title'].str.strip()
     return df
+
+
+def remove_unnecessary_columns(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.drop(columns=['Obciążenia', 'Uznania', 'Odbiorca/Zleceniodawca'])
+    return df
+
+
+def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
+    df = df.rename(columns={'Data transakcji': 'Date', 'Waluta': 'Currency'})
+    return df
+
+
+def format_date_column(df: pd.DataFrame) -> pd.DataFrame:
+    df['Date'] = pd.to_datetime(df.Date)
+    df['Date'] = df['Date'].dt.date
+    return df
+
+
+def add_account_id_to_each_row(df: pd.DataFrame, account_id: str) -> pd.DataFrame:
+    df['Account'] = account_id
+    df['Account'] = df['Account'].astype('string')
+    return df
+
+
+def add_details_and_balance_columns(df: pd.DataFrame) -> pd.DataFrame:
+    df['Details'] = np.NaN
+    df['Balance'] = np.NaN
+    return df
+
+
+def add_empty_columns(df: pd.DataFrame, columns) -> pd.DataFrame:
+    existing_columns = list(df.columns)
+    return df.reindex(columns=existing_columns + columns)
+
+
+def reverse_rows_order(df: pd.DataFrame) -> pd.DataFrame:
+    return df.iloc[::-1]
+
+
+def order_columns(
+        df: pd.DataFrame,
+        columns_in_order: list[str]) -> pd.DataFrame:
+    return df[columns_in_order]
+
 
 class Millenium(models.Importer):
     # Millenium bank (PL) - https://www.bankmillennium.pl
@@ -21,26 +82,16 @@ class Millenium(models.Importer):
         return pd.read_csv(io.StringIO(contents.decode('utf-8')))
 
     def format_file(self, df: pd.DataFrame, account_id: str):
-        df = df[['Data transakcji', 'Odbiorca/Zleceniodawca', 'Opis', 'Obciążenia', 'Uznania', 'Waluta']]
+        df = df.pipe(select_only_required_columns)\
+            .pipe(get_operation)\
+            .pipe(prepare_tile)\
+            .pipe(remove_unnecessary_columns)\
+            .pipe(rename_columns)\
+            .pipe(format_date_column)\
+            .pipe(add_account_id_to_each_row, account_id)\
+            .pipe(add_details_and_balance_columns)\
+            .pipe(add_empty_columns, ['Category', 'Comment'])\
+            .pipe(reverse_rows_order)\
+            .pipe(order_columns, db.account_columns)
 
-        df['Operation'] = np.where(df['Obciążenia'] < 0, df['Obciążenia'], df['Uznania'])
-        df = df.drop(columns=['Obciążenia', 'Uznania'])
-
-        df = df.pipe(prepare_tile)
-        df = df.rename(columns={'Data transakcji': 'Date', 'Waluta': 'Currency'})
-
-        df['Date'] = pd.to_datetime(df.Date)
-        df['Date'] = df['Date'].dt.date
-        df['Account'] = account_id
-        df['Account'] = df['Account'].astype('string')
-        df['Details'] = np.NaN
-        df['Balance'] = np.NaN
-
-        result = self.__add_missing_columns(df, ['Category', 'Comment'])
-        result = result.iloc[::-1]
-        result = result[db.account_columns]
-        return result
-
-    def __add_missing_columns(self, df: pd.DataFrame, columns):
-        existing_columns = list(df.columns)
-        return df.reindex(columns=existing_columns + columns)
+        return df
