f54d0dbf58a3c80096ec15a06ee9c73a92ab9417
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2024-07-31 19:46:26 +0000

    select app profile

--- services/mankkoo/mankkoo/app.py
+++ services/mankkoo/mankkoo/app.py
@@ -1,13 +1,15 @@
 import os
 
 from apiflask import APIFlask
+from flask_cors import CORS
+
 from mankkoo.controller.main_controller import main_endpoints
 from mankkoo.controller.account_controller import account_endpoints
 from mankkoo.util import config
-
+from mankkoo.base_logger import log
 import mankkoo.config as app_profile
 import mankkoo.database as db
-from flask_cors import CORS
+
 
 app = APIFlask(__name__, title="Mankkoo - 'accounting' service API", version='1.0', spec_path='/openapi.yaml', docs_ui='elements')
 CORS(app)
@@ -26,6 +28,9 @@ app.config['INFO'] = {
 db_name = ''
 
 # to enable 'prod' config first run: export FLASK_ENV=prod
+app.config["ENV"] = os.getenv("FLASK_ENV", "dev")
+log.info(f'Starting mankkoo backend service with profile: {app.config["ENV"]}')
+
 if app.config["ENV"] == 'prod':
     app.config.from_object('mankkoo.config.ProdConfig')
     db_name = app_profile.ProdConfig.DB_NAME
