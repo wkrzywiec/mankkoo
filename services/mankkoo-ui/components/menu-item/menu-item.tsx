886499c4edbba96f64f66834547a2b2486606c19
Wojciech Krzywiec <wojciech.krzywiec@sartorius.com>
2024-10-11 14:17:59 +0000

    menu icons

--- services/mankkoo-ui/components/menu-item/menu-item.tsx
+++ services/mankkoo-ui/components/menu-item/menu-item.tsx
@@ -1,10 +1,15 @@
 import Link from 'next/link'
 import classes from './menu-item.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { IconDefinition } from '@fortawesome/fontawesome-common-types'
 
-export default function MenuItem({text, link}: {text: string, link: string}) {
+export default function MenuItem({text, link, icon}: {text: string, link: string, icon: IconDefinition | undefined}) {
     return (
         <li className={classes.menuItem}>
-            <Link href={link}>{text}</Link>
+            <Link href={link}>
+                <FontAwesomeIcon icon={icon}/>
+                <p>{text}</p>
+            </Link>
         </li>
     )
 }
--- services/mankkoo-ui/components/menu/menu.tsx
+++ services/mankkoo-ui/components/menu/menu.tsx
@@ -1,18 +1,22 @@
 import classes from './menu.module.css'
 import MenuItem from '@/components/menu-item/menu-item'
 
+import { 
+    faGaugeHigh, faMoneyBillWave, faChartLine,
+    faChessRook, faBuildingUser, faServer } from '@fortawesome/free-solid-svg-icons'
+
 export default function Menu() {
     return (
         <div className={classes.menu}>
             <nav>
                 <ul>
-                    <MenuItem text="Mankkoo" link="/" />
-                    <MenuItem text="Overview" link="/" />
-                    <MenuItem text="Accounts" link="/accounts" />
-                    <MenuItem text="Investments" link="/investments" />
-                    <MenuItem text="Financial Fortress" link="/fortress" />
-                    <MenuItem text="Real Estate" link="/estate" />
-                    <MenuItem text="Streams" link="/streams" />
+                    <MenuItem text="Mankkoo" link="/" icon={undefined}/>
+                    <MenuItem text="Overview" link="/" icon={faGaugeHigh}/>
+                    <MenuItem text="Accounts" link="/accounts" icon={faMoneyBillWave}/>
+                    <MenuItem text="Investments" link="/investments" icon={faChartLine}/>
+                    <MenuItem text="Financial Fortress" link="/fortress" icon={faChessRook}/>
+                    <MenuItem text="Real Estate" link="/estate" icon={faBuildingUser}/>
+                    <MenuItem text="Streams" link="/streams" icon={faServer}/>
                 </ul>
             </nav>  
         </div>
