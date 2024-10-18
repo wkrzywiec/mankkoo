f2da94393328a7b592a6ca0f455d274f32ec9c1a
Wojciech Krzywiec <wojciech.krzywiec@sartorius.com>
2024-10-18 13:00:10 +0000

    main page h1 and h2

--- services/mankkoo-ui/app/page.tsx
+++ services/mankkoo-ui/app/page.tsx
@@ -3,45 +3,59 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.mainContainer}>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
+      <div className={`${styles.gridItem} ${styles.span2Columns}`}>
+        <h1>Finance Overview</h1>
+        <p>Detailed overview of your personal finances, summarizing your current wealth, displaying historical data, and highlighting trends.</p>
+      </div>
+      <div className={styles.span2Columns}>
+      </div>
 
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
+      <div className={`${styles.gridItem} ${styles.span2Columns}`}>
+        <h2>Current Indicators</h2>
+      </div>
+      <div className={styles.span2Columns}>
+      </div>
+      
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
 
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
 
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
+      <div className={`${styles.gridItem} ${styles.span2Columns}`}>
+        <h2>History</h2>
+      </div>
+      <div className={styles.span2Columns}>
+      </div>
 
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
 
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
 
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
 
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
-      <div className={styles.gridItem}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
+      <div className={`${styles.gridItem} ${styles.dummyGridItem}`}></div>
     </main>
   );
 }
