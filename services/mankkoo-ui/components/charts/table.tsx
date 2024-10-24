b23fcd5e30d17bc7d20761b9b2b215ee83890582
Wojciech Krzywiec <wojciech.krzywiec@sartorius.com>
2024-10-24 13:51:49 +0000

    table style

--- services/mankkoo-ui/components/charts/table.tsx
+++ services/mankkoo-ui/components/charts/table.tsx
@@ -1,6 +1,12 @@
+import styles from "./table.module.css";
 
-export default function Table() {
-    const withSummary = true;
+import { CSSProperties } from 'react';
+
+function shouldBoldLastRow(data: string[][], rowIndex: number, boldLastRow: boolean): boolean {
+    return boldLastRow && rowIndex + 1 == data.length;
+}
+
+export default function Table({style, boldLastRow=false}: {style?: CSSProperties, boldLastRow?: boolean}) {
     const data = [
         ["01", "Checking accounts", "50 000 PLN", "85%"],
         ["02", "Savings accounts", "5 000 PLN", "5%"],
@@ -10,15 +16,17 @@ export default function Table() {
     ]
 
     const rows = data.map((rowData, rowIndex) => 
-        <tr key={rowIndex}>
-            { rowData.map(
-                (cellData, cellIndex) => <td key={rowIndex + "_" + cellIndex}>{cellData}</td>)
-            }
+        <tr key={rowIndex} className={shouldBoldLastRow(data, rowIndex, boldLastRow) ? styles.boldedRow : styles.row}>
+            { rowData.map((cellData, cellIndex) => 
+                <td key={rowIndex + "_" + cellIndex}>{cellData}</td>
+            )}
         </tr>  
     )
     return (
-        <table>
-            {rows}
+        <table style={style} className={styles.table}>
+            <tbody>
+                {rows}
+            </tbody>
         </table>
     )
 }
