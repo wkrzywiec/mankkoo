969323b47e2c33b74bd30b5dc06220d59a318e19
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2024-11-07 07:03:41 +0000

    update file names

--- services/mankkoo-ui/app/layout.tsx
+++ services/mankkoo-ui/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from 'next/font/google'
 
-import Menu from '@/components/menu/menu'
+import Menu from '@/components/menu/Menu'
 
 
 const inter = Inter({ subsets: ['latin'] })
