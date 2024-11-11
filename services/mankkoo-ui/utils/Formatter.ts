f891a89cca0950b0602af7369cb06c071afaa72d
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2024-11-11 11:54:54 +0000

    currency format

--- services/mankkoo-ui/utils/Formatter.ts
+++ services/mankkoo-ui/utils/Formatter.ts
@@ -1,9 +1,23 @@
-export function currencyFormat(value: number | undefined): string {
-    const number = value === undefined ? 0 : value;
-    return new Intl.NumberFormat('pl-PL', {
-      style: 'currency',
-      currency: 'PLN',
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2
-    }).format(number);
+export function currencyFormat(value: number | string | undefined): string {
+  
+  let number: number;
+  
+  if (typeof value === 'string') {
+    number = value === undefined ? 0 : parseFloat(value);
+  } else {
+    number = value === undefined ? 0 : value;
+  }
+
+  
+  return new Intl.NumberFormat('pl-PL', {
+    style: 'currency',
+    currency: 'PLN',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(number);
+}
+
+export function percentage(value: number | undefined): string {
+  const per = value === undefined ? 0 : value;
+  return per.toLocaleString("pl-PL", {style: "percent", maximumFractionDigits: 2});
 }
