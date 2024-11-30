312b4c5ccf863430f876cbc1bdd45189670746e3
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2024-11-30 21:07:17 +0000

    menuItem

--- services/mankkoo-ui/components/menu/MenuItem.tsx
+++ services/mankkoo-ui/components/menu/MenuItem.tsx
@@ -3,11 +3,12 @@ import classes from './MenuItem.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { IconDefinition } from '@fortawesome/fontawesome-common-types'
 
-export default function MenuItem({text, link, icon}: {text: string, link: string, icon: IconDefinition | undefined}) {
+export default function MenuItem({text, link, icon}: {text: string, link: string, icon?: IconDefinition}) {
+    const fontAwesomeIcon = (icon !== undefined) ? <FontAwesomeIcon icon={icon}/> : ""
     return (
         <li className={classes.menuItem}>
             <Link href={link}>
-                <FontAwesomeIcon icon={icon}/>
+                {fontAwesomeIcon}
                 <p>{text}</p>
             </Link>
         </li>
