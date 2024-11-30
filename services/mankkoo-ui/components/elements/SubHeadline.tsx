44e07c61bc430a7e8813b5aae05c8817bdc9baf6
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2024-11-30 20:56:01 +0000

    TileHeader fix

--- services/mankkoo-ui/components/elements/SubHeadline.tsx
+++ services/mankkoo-ui/components/elements/SubHeadline.tsx
@@ -1,6 +1,7 @@
+import { ReactNode } from "react"
 import classes from "./SubHeadline.module.css"
 
-export default function SubHeadline({text}: {text: string}) {
+export default function SubHeadline({text}: {text: string | ReactNode}) {
     return (
         <p className={classes.tileText}>
             {text}
--- services/mankkoo-ui/components/elements/TileHeader.tsx
+++ services/mankkoo-ui/components/elements/TileHeader.tsx
@@ -2,7 +2,7 @@ import Headline from "@/components/elements/Headline"
 import SubHeadline from "@/components/elements/SubHeadline"
 import { ReactNode } from 'react';
 
-export default function TileHeader({headline, subHeadline, headlineElement}: {headline?: string, subHeadline?: string | ReactNode, headlineElement?: ReactNode}) {
+export default function TileHeader({headline, subHeadline, headlineElement}: {headline: string, subHeadline: string | ReactNode, headlineElement?: ReactNode}) {
     return (
         <>
             <Headline text={headline} fontSize="2">{headlineElement}</Headline>
