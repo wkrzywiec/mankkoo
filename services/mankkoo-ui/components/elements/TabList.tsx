c9b4a3426d98e1059d1993ffcbeec8c0820642a4
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2025-02-15 15:42:23 +0000

    fix type error

--- services/mankkoo-ui/components/elements/TabList.tsx
+++ services/mankkoo-ui/components/elements/TabList.tsx
@@ -17,24 +17,28 @@ export default function TabList({children, activeTabIndex = 0}: {children: React
     const handleTabClick = (index: number) => {
         setActiveTab(index);
     };
+
+    const labels = children
+        .map(tab => tab.props as { [key: string]: any })
+        .map(tabProps => tabProps.label as string)
     
     return (
         <div className="tabs">
             <nav className="tab-list-wrapper">
                 <ul className="tab-list" role="tablist" aria-orientation="horizontal">
-                {children.map((tab, index) => (
+                {labels.map((label, index) => (
                     <li key={`tab-${index}`}>
                         <button
                             key={`tab-btn-${index}`}
                             role="tab"
-                            id={`tab-${sanitizeForId(tab.props.label)}`}
-                            aria-controls={`panel-${sanitizeForId(tab.props.label)}`}
+                            id={`tab-${sanitizeForId(label)}`}
+                            aria-controls={`panel-${sanitizeForId(label)}`}
                             aria-selected={activeTab === index}
                             onClick={() => handleTabClick(index)}
                             className={`tab-btn ${
                                 activeTab === index && "tab-btn--active"
                             }`}
-                        >{tab.props.label}</button>
+                        >{label}</button>
                     </li>
                 ))}
                 </ul>
