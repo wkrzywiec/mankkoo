dca6178b22401a25df140520ca85710257e0155b
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2025-05-21 18:22:42 +0000

    investment page scaffolding

--- services/mankkoo-ui/app/investments/page.tsx
+++ services/mankkoo-ui/app/investments/page.tsx
@@ -7,6 +7,7 @@ import dynamic from 'next/dynamic';
 import Indicator from "@/components/elements/Indicator"
 import TileHeader from "@/components/elements/TileHeader";
 import PieChart from "@/components/charts/Piechart";
+import TabList from "@/components/elements/TabList";
 
 const LineChart = dynamic(() => import('@/components/charts/Line'), {
   ssr: false, // Disable server-side rendering, more info: https://nextjs.org/docs/messages/react-hydration-error
@@ -17,6 +18,35 @@ const Table = dynamic(() => import('@/components/charts/Table'), {
 });
 
 export default function Investments() {
+
+  const changeTab = (index: number) => {
+    
+
+    return <div className="mainContainer">
+      <div className="gridItem span2Columns span2Rows">
+        <TileHeader headline="Investments" subHeadline="Lits of active investments in a wallet." />
+        <Table hasHeader={true} style={{ width: "90%" }} boldLastRow={false} currencyColumnIdx={-1} colorsColumnIdx={-1}/>
+      </div>
+      <div className="gridItem span2Columns">
+        <TileHeader headline="Diversification" subHeadline="Wallet composition by asset type" />
+        <div className={styles.horizontalAlignment}>
+          <PieChart />
+          <Table />
+        </div>
+      </div>
+      <div className="gridItem span2Columns">
+        <TileHeader headline="History" subHeadline="History of wallet's results." />
+        <LineChart />
+      </div>
+
+      <div className="gridItem span4Columns">
+        <TileHeader headline="Transactions" subHeadline="... - Log of all transactions for the selected investment (to view a different investment, choose one from the table above)."/>
+        <Table hasHeader={true} style={{ width: "90%" }} boldLastRow={false} currencyColumnIdx={-1} colorsColumnIdx={-1}/>
+      </div>
+  </div>
+  }
+
+
   return (
     <main className="mainContainer">
       <div className="gridItem span4Columns">
@@ -53,8 +83,19 @@ export default function Investments() {
       </div>
 
       <div className="gridItem span4Columns">
-        <TileHeader headline="Investments History" subHeadline="Illustrates the historical growth of total invested funds over time."/>
+        <TileHeader headline="Investments History" subHeadline="📈 Illustrates the historical growth of total invested funds over time."/>
         <LineChart />
+      </div>
+
+      <div className="gridItem span4Columns">
+        <TileHeader headline="Wallets" subHeadline="💼 Shows investments by wallet, with insights, composition, history, and recent operations."/>
+      </div>
+
+      <div className="gridItem span4Columns">
+        <TabList
+          labels={['Accounts', 'Investments', 'Stocks', 'Retirement', 'Real Estate', 'Inactive Streams']} 
+          tabContent={(index) => changeTab(index)}
+        />
       </div>
     </main>
   );
