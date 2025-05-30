df68910fb5b2b37e145a7d958758d009d2ffa178
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2025-05-30 06:12:27 +0000

    fix test

--- services/mankkoo/tests/controller/admin_controller_test.py
+++ services/mankkoo/tests/controller/admin_controller_test.py
@@ -10,7 +10,7 @@ def test_all_views_are_returned(test_client):
 
     payload = response.get_json()
     assert 'views' in payload
-    assert len(payload['views']) == 5
+    assert len(payload['views']) == 6
     assert views.main_indicators_key in payload['views']
     assert views.current_savings_distribution_key in payload['views']
     assert views.total_history_per_day_key in payload['views']
