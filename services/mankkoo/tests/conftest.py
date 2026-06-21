520110b479488aa18ab38e5494af89c1a2e2a089
Wojtek Krzywiec <wojciechkrzywiec@gmail.com>
2026-06-21 20:17:21 +0200

    corrections

--- services/mankkoo/tests/conftest.py
+++ services/mankkoo/tests/conftest.py
@@ -30,7 +30,7 @@ def setup(request):
         request.addfinalizer(remove_container)
         os.environ["DB_CONN"] = postgres.get_connection_url()
         os.environ["DB_HOST"] = postgres.get_container_host_ip()
-        os.environ["DB_PORT"] = postgres.get_exposed_port(5432)
+        os.environ["DB_PORT"] = str(postgres.get_exposed_port(5432))
         os.environ["DB_USERNAME"] = postgres.username
         os.environ["DB_PASSWORD"] = postgres.password
         os.environ["DB_NAME"] = postgres.dbname
